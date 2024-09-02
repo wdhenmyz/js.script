4f81f7e91f6895e0ee4133059fd4128799b3b86c
wesllen dhenmyz brito ferreira <wesllen.dhenmyz@outlook.com>
2024-09-02 11:13:07 -0300

    Update function_3.js

--- cisco.function/function_3.js
+++ cisco.function/function_3.js
@@ -1,1 +1,13 @@
+// setTimeout e setInterval
+
+/*
+  A função setTimeout é usada quando você deseja causar uma ação atrasada. Uma função semelhante é setInterval. 
+  Desta vez, a ação também é realizada com um atraso, mas periodicamente, então ela é repetida em intervalos fixos.
+  Enquanto isso, o programa principal é executado, e a cada tempo especificado, o retorno de chamada dado como argumento para uma chamada setInterval é chamado.
+*/
+
+
+
+
+
 
