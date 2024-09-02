4526bb6773037832eb74a0c7fe779e08cd698682
wesllen dhenmyz brito ferreira <wesllen.dhenmyz@outlook.com>
2024-09-02 11:56:35 -0300

    Update function_pratica.js

--- cisco.function/function_pratica.js
+++ cisco.function/function_pratica.js
@@ -88,6 +88,74 @@ let add = (a, b) => !Number.isInteger(a) || !Number.isInteger(b) ? NaN : a + b;
 let sub = (a, b) => !Number.isInteger(a) || !Number.isInteger(b) ? NaN : a - b;
 let mult = (a, b) => !Number.isInteger(a) || !Number.isInteger(b) ? NaN : a * b;
 
+/*
+  Exercício 4: Escreva uma função action que receberá a função de retorno de chamada como seu primeiro argumento e os outros dois argumentos como números.
+  Como uma função de retorno de chamada, você poderá passar uma das três funções da tarefa anterior.
+  A função action chamará a função de retorno de chamada passada a ela e retornará o resultado obtido. 
+  A função de retorno de chamada aceitará o segundo e o terceiro argumentos da chamada de ação.
+*/
+
+let action = (callback, a, b) => callback(a, b);
+// or
+let action = function (callback, a, b) {
+    return callback(a, b);
+}
+// or
+function action(callback, a, b) {
+    return callback(a, b);
+}
+
+// Exercício 5: Escreva um programa que imprima (no console) inteiros consecutivos 10 vezes, em intervalos de dois segundos (comece com o número 1).
+// Use as funções setInterval, clearInterval and setTimeout.
+
+let counter = 1;
+let intervalId = setInterval(function () {
+    console.log(counter++);
+}, 2000);
+setTimeout(function () {
+    clearInterval(intervalId)
+}, 20000);
+
+/*
+  Exercício 6: Escreva uma função que irá calcular o n-ésimo elemento da sequência de Fibonacci.
+  Então, cada elemento da sequência (exceto os dois primeiros) é a soma dos dois anteriores. 
+  Por exemplo: F1 = 1, F2 = F1 + F0 = 1, F3 = F2 + F1 = 2 e F6 = F5 + F4 = 8 . 
+  A função deve usar recursão . Na definição, use uma expressão de função (armazene uma função anônima em uma variável).
+*/
+
+let fibbRec = function (n) {
+    let retVal = 0;
+    if (n != 0) {
+        if (n === 1) {
+            retVal = 1;
+        } else {
+            retVal = fibbRec(n - 1) + fibbRec(n - 2);
+        }
+    }
+    return retVal;
+}
+
+console.log(fibbRec(4)); // -> 3
+console.log(fibbRec(7)); // -> 13
+
+// Exercício 7: re-escreva a função do exercício 5 usando função de flecha, mas simplifique o máximo que puder
+
+let fibb = n => n == 0 ? 0 : (n == 1 ? 1 : fibb(n - 1) + fibb(n - 2));
+console.log(fibb(4)); // -> 3
+console.log(fibb(7)); // -> 13
+
+// Exercício 8: Escreva uma versão iterativa da função do Exercício 5 (use o loop for). Declare a função usando uma instrução de função.
+
+function fibbIter(n) {
+    let a = 0, b = 1;
+    for (let i = 2; i <= n; i++) {
+        let c = a;
+        a = b;
+        b += c;        
+    }
+    return b;
+}
+
 
 
 
