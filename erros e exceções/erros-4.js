a0f602689ec2e37948ae6c5980ace2218d2e2fdc
wesllen dhenmyz brito ferreira <wesllen.dhenmyz@outlook.com>
2024-09-05 15:40:29 -0300

    Update erros-4.js

--- erros e exceções/erros-4.js
+++ erros e exceções/erros-4.js
@@ -104,3 +104,94 @@ try {
 }
 // Neste exemplo, capturamos a exceção dentro do bloco catch colocando o código dentro de outra instrução try...catch.
 
+
+// A declaração throw e erros personalizados
+/*
+  Há várias razões pelas quais você gera suas próprias exceções. 
+  A maioria delas é bastante complexa e não muito útil neste estágio de aprendizado. 
+  A situação mais fácil de imaginar é quando você escreve uma função própria, que deve sinalizar os dados incorretos que foram passados ​​para ela.
+
+  Para lançar uma exceção, usamos a instrução throw. 
+  Ela é seguida por qualquer valor que será tratado como uma exceção. 
+  Pode ser, por exemplo, um número ou um dos objetos de erro prontos (por exemplo, RangeError).
+
+  Uma exceção que lançamos fará com que o programa reaja da mesma forma que as exceções originais do JavaScript (ou seja, ele interromperá sua execução).
+  Isto é, a menos que a lancemos dentro do bloco try para lidar com isso.
+  Vamos dar uma olhada em um exemplo simples, sem tentar encontrar nenhum significado especial nele. Esta é apenas uma ilustração do uso da instrução throw:  */
+
+console.log("start"); // -> start
+throw 100; // -> Uncaught 100
+console.log("end");
+// Uma exceção não suportada (se o número 100 pode ser chamado de exceção) faz com que o programa pare. A segunda instrução console.log nunca é executada.
+
+// Vamos fechar a instrução throw dentro do bloco try:
+console.log("start"); // -> start
+try {
+    throw 100;
+} catch (error) {
+    console.log(error); // -> 100
+}
+console.log("end"); // -> end
+/*  Desta vez, nossa exceção é capturada e manipulada no bloco catch, e não interrompe a execução posterior.
+  
+  outro exemplo
+
+  Sonhamos com uma função que nos permitirá contar fatoriais (espero que você ainda se lembre do que é um fatorial das suas aulas de matemática;
+  se estiver em dúvida, dê uma olhada rápida na Wikipedia para ver um exemplo). 
+  Vamos escrevê-la em uma versão iterativa, ou seja, usando um loop.
+  Não será nem a solução mais elegante, nem a mais ótima, mas simples e legível.  */
+
+function factorial(n) {
+    let result = 1;
+    for (; n > 1; n--) {
+        result = result * n;
+    }
+    return result;
+}
+ 
+console.log(factorial(3)); // -> 6
+console.log(factorial(5)); // -> 120
+console.log(factorial(8)); // -> 40320
+console.log(factorial(20)); // -> 2432902008176640000
+console.log(factorial(1000)); // -> Infinity
+
+// Digamos que estamos um pouco assustados com os números grandes retornados pela nossa função, especialmente o valor Infinity,
+// então decidimos limitar o intervalo máximo de valores suportados. Não aceitaremos argumentos maiores que 20.
+Function factorial(n) {
+    if (n > 20) {
+        throw new RangeError("Max value 20");
+    }
+    let result = 1;
+    for (; n > 1; n--) {
+        result = result * n;
+    }
+    return result;
+}
+ 
+console.log(factorial(20)); // -> 2432902008176640000
+console.log(factorial(1000)); // -> Uncaught RangeError: Max value 20
+
+/*  A presença de uma instrução condicional dentro da nossa função é bastante óbvia. 
+  Assim como o uso da instrução throw. 
+  A construção new RangeError("Max value 20") definitivamente precisa de uma explicação. 
+  Está um pouco fora do escopo desta parte do curso, então tentaremos explicá-la da maneira mais simples possível, focando apenas no seu lado funcional.
+
+  Como mencionamos anteriormente, a instrução throw pode assumir qualquer valor. Anteriormente,
+  usávamos um número simples, mas desta vez estamos buscando algo mais complexo. 
+  É um objeto, que é um tipo de dado composto. Você pode criar um novo objeto de muitas maneiras, inclusive usando o operador new.
+  Usando esse operador, criamos um objeto de classe RangeError, que é um erro predefinido que discutimos há algum tempo.
+  O novo objeto é iniciado pela string Max value 20.
+  E um novo objeto, do tipo RangeError, contendo, entre outras coisas, a string que fornecemos, será lançado se o parâmetro n exceder o valor permitido.  */
+
+
+
+
+
+
+
+
+
+
+
+
+
