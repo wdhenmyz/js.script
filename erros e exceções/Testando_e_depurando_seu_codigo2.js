91d92507571a392d9c8171d64a12acc23484abea
wesllen dhenmyz brito ferreira <wesllen.dhenmyz@outlook.com>
2024-09-06 19:34:13 -0300

    Update Testando_e_depurando_seu_codigo2.js

--- erros e exceções/Testando_e_depurando_seu_codigo2.js
+++ erros e exceções/Testando_e_depurando_seu_codigo2.js
@@ -1,1 +1,37 @@
+// Como lidar sem a declaração do depurador
+
+/*  Novamente, modifique o programa salvo em main.js, desta vez removendo a linha que contém o comando debugger. 
+  Salve as alterações, volte para o seu navegador e recarregue a página. Obviamente, o programa foi executado até o fim, mas agora sabemos como pará-lo. 
+  Defina dois pontos de interrupção, um em console.log("before outer() call"); o outro em console.log("after outer() call"); (agora devem ser as linhas 12 e 14, respectivamente).
+  Recarregue a página. O programa deve parar no primeiro ponto de interrupção. 
+  Clicar em Resume fará com que o programa retome a execução e pare no segundo ponto de interrupção. 
+
+  Outro clique em Resume  fará com que o programa seja executado até o fim.  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
