2df909fa41f129905120bdb0efe4950dcac019a0
wesllen dhenmyz brito ferreira <wesllen.dhenmyz@outlook.com>
2024-09-07 14:50:29 -0300

    Update Testando_e_depurando_seu_codigo2.js

--- erros e exceções/Testando_e_depurando_seu_codigo2.js
+++ erros e exceções/Testando_e_depurando_seu_codigo2.js
@@ -8,8 +8,40 @@
 
   Outro clique em Resume  fará com que o programa seja executado até o fim.  */
 
+/*  Além de saltar entre pontos de interrupção sucessivos com Resume , 
+  temos a possibilidade de executar uma execução passo a passo real (ou seja, chamar as instruções do nosso programa uma de cada vez). 
+  Há um pequeno problema aqui. 
+  Se uma instrução for uma chamada para uma função, 
+  o depurador deve entrar na função e executar as instruções dentro dela passo a passo, ou tratá-la como um todo indivisível e apenas executá-la?
+  Claro, não há uma resposta correta, e tudo dependerá da situação específica e do que queremos alcançar.
+  É por isso que os depuradores distinguem entre dois modos de execução passo a passo: 
+  Step Into (tratando a função como um conjunto de instruções, que queremos executar separadamente) e 
+  Step Over (tratando a chamada de função como algo indivisível).  */
+
+/*  Step over
+  Remova o segundo ponto de interrupção (da última linha do código) e recarregue a página. 
+  Localize o botão Step Over (à direita de Resume, a seta formando um arco sobre o ponto). 
+  Pressione-o – o destaque no código deve se mover para a próxima linha após o ponto de interrupção. 
+  Ao mesmo tempo, o console mostrará o efeito da instrução que você acabou de executar.
+  Pressione Step Over mais duas vezes (alternativamente, use o atalho F10) observando as alterações no console e o destaque do código.
+
+    Step into
+  Vamos ver qual é a diferença entre Step Over e Step Into na prática. 
+  Deixe a configuração do ponto de interrupção inalterada e recarregue a página. 
+  Primeiro execute Step Over (pressione o botão ou o atalho F10). 
+  Então, quando paramos na linha console.log(outer()), execute Step Into.
+
+  O que acontece? Desta vez, o depurador trata a função externa como um conjunto de instruções, 
+  pula para dentro dela e se define em sua primeira linha. 
+  Usando Step Into, vá mais para dentro da função interna e pare na linha return "Hello !".
+
+    call stack
+  
 
 
+
+
+    step out
 
 
 
