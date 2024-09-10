a5cd5892518d671e87e7b2128ab89488d1844e6f
wesllen dhenmyz brito ferreira <wesllen.dhenmyz@outlook.com>
2024-09-10 10:55:39 -0300

    Create começando javascript-pratica.js

---
+++ começando javascript-pratica.js
@@ -0,0 +1,29 @@
+/*  Pergunta 1: Use console.log para enviar seu nome completo para o console.  */
+
+  
+/*  Pergunta 2: Envie seu ano de nascimento.  */
+
+  
+/*  Pergunta 3: Tente enviar seu ano de nascimento novamente, dessa vez passando a data sem as aspas.  */
+
+  
+  Pergunta 4: Podemos passar vários argumentos para console.log separados por vírgulas, por exemplo:
+  
+  console.log("abc", "def", "ghi");
+  Envie informações sobre você para o console no formato: Nome Sobrenome (Ano) por exemplo Mary Stuart (1542).
+  
+  fornecendo todas as informações como um argumento;
+  fornecendo o nome, sobrenome e ano como argumentos separados.
+    
+  
+/*  Pergunta 5: Envie as mesmas informações (nome, sobrenome, ano) para o console, não lado a lado, mas em linhas consecutivas.  */
+    
+  
+/*  Pergunta 6: Uma string pode ser concatenada usando o sinal +, por exemplo "abc" + "def" será tratado como "abcdef".  */
+    
+
+
+
+
+
+/**/
