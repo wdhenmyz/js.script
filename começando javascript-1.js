732c6cf644e2e9853d6cd724370d1acb564cdd93
wesllen dhenmyz brito ferreira <wesllen.dhenmyz@outlook.com>
2024-09-10 10:42:51 -0300

    Update começando javascript-1.js

--- começando javascript-1.js
+++ começando javascript-1.js
@@ -17,6 +17,8 @@
 
 console.log("Hello, World!");
 // nesse caso a função é apenas um log, e console é o objeto ao qual a função pertence.
+// No editor, você deve ver o pedaço de código que foi discutido, contendo a função console.log, Tente executá-lo.
+// Como resultado, a janela inferior simulando o console deve mostrar:  Hello, World!
 
 /*  A função é um pedaço de código que permite que você execute uma tarefa específica (no nosso caso, exibir algo no console). 
   Funções geralmente recebem argumentos, em outras palavras, dados que elas usarão durante a operação. 
@@ -27,6 +29,33 @@ console.log("Hello, World!");
   Para que o interpretador saiba onde o comando termina, colocamos um ponto e vírgula no final da chamada da função. 
   Nesse caso, o interpretador conseguiria sem essa ajuda, mas é um bom hábito terminar cada comando com um ponto e vírgula, 
   para que você não se esqueça dele quando for realmente necessário.  */
+
+// nota sobre html e javascript
+/*  O código JavaScript a ser executado pelo navegador na página deve ser anexado ao HTML usando a tag <script>, 
+  e há duas maneiras de fazer isso. O código pode ser incorporado diretamente dentro das tags <script> e </script>, mas isso só é recomendado quando o código é curto. 
+  Outra abordagem é usar o atributo "src" para apontar para um arquivo separado que contém o código JavaScript. 
+  Isso é especialmente verdadeiro quando o mesmo código vai ser usado em várias páginas, 
+  porque repetir exatamente o mesmo código muitas vezes é uma prática ruim, pois quaisquer alterações precisam ser aplicadas a todos os arquivos; 
+  e, além disso, aumenta artificialmente o tamanho da página. 
+  A extensão do arquivo JavaScript é .js.  
+
+  Esse comportamento pode ser alterado para scripts externos apontados pelo atributo "src" usando os atributos "defer" ou "async".
+  > defer – significa que o script deve ser executado após a página inteira ser carregada;
+  > async – significa que o script será executado imediatamente, mas em paralelo à análise do resto da página.  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
