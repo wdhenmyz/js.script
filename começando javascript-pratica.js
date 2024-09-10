ce4a7db093e4fdec2640a115374640ca8f86c221
wesllen dhenmyz brito ferreira <wesllen.dhenmyz@outlook.com>
2024-09-10 10:59:26 -0300

    Update começando javascript-pratica.js

--- começando javascript-pratica.js
+++ começando javascript-pratica.js
@@ -1,29 +1,30 @@
 /*  Pergunta 1: Use console.log para enviar seu nome completo para o console.  */
-
+  console.log("Mary Stuart");
   
 /*  Pergunta 2: Envie seu ano de nascimento.  */
-
+  console.log("1542");
   
 /*  Pergunta 3: Tente enviar seu ano de nascimento novamente, dessa vez passando a data sem as aspas.  */
-
+  console.log(1542);
   
-  Pergunta 4: Podemos passar vários argumentos para console.log separados por vírgulas, por exemplo:
+/*  Pergunta 4: Podemos passar vários argumentos para console.log separados por vírgulas, por exemplo:
   
   console.log("abc", "def", "ghi");
   Envie informações sobre você para o console no formato: Nome Sobrenome (Ano) por exemplo Mary Stuart (1542).
   
   fornecendo todas as informações como um argumento;
-  fornecendo o nome, sobrenome e ano como argumentos separados.
-    
+  fornecendo o nome, sobrenome e ano como argumentos separados.  */
+  console.log("Mary Stuart (1542)");
+  console.log("Mary", "Stuart", "(1542)");
   
 /*  Pergunta 5: Envie as mesmas informações (nome, sobrenome, ano) para o console, não lado a lado, mas em linhas consecutivas.  */
-    
+  console.log("Mary");
+  console.log("Stuart");
+  console.log("(1542)");
   
-/*  Pergunta 6: Uma string pode ser concatenada usando o sinal +, por exemplo "abc" + "def" será tratado como "abcdef".  */
-    
+/*  Pergunta 6: Uma string pode ser concatenada usando o sinal +, por exemplo "abc" + "def" será tratado como "abcdef".
+  Tente escrever seu nome, sobrenome e ano de nascimento em uma linha novamente, dessa vez não separados por vírgulas, mas por sinais +.  */  
+  console.log("Mary" + "Stuart" + "(1542)");
 
-
-
-
-
-/**/
+// Pergunta 7: Coloque espaços nos lugares apropriados, para que quando exibidos, você obtenha o mesmo efeito da Pergunta 4.
+  console.log("Mary" + "Stuart" + "(1542)");
